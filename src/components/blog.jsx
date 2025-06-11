31c1b10704dd53cf395a422ee742f71e3e22d126
priyankadashman21 <priyankadashman8@gmail.com>
2025-06-11 15:23:42 +0530

    updates

--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -3,7 +3,7 @@ import blogImage from "../assets/blogpost.jpg";
 
 const Blog = () => {
   return (
-    <div className="flex w-[440px] p-9 px-4 flex-col items-center gap-5 bg-[#FAFAFA]">
+    <div className="flex w-[100%] p-9 px-4 flex-col items-center gap-5 bg-[#FAFAFA]">
       <div className="flex flex-col items-start gap-2 w-full">
         <h2 className="w-full text-[#323232] text-center font-inter text-xl font-bold leading-[30px]">
           Insights That Power Your Global Journey
@@ -13,14 +13,13 @@ const Blog = () => {
         </p>
     
         </div>
-         <div className="flex flex-col items-start gap-5 h-[772px] w-[408px]">
-           <div className="flex w-[408px] h-[376px] p-4 flex-col items-start gap-4 rounded-[16px] border-2 border-[#88D1D8] bg-[linear-gradient(133deg,_#FFF_1.02%,_#C5E6E9_99.79%)] shadow-[2.769px_6.646px_12.837px_0px_rgba(0,0,0,0.04)]">
-  <div className="h-[200px] w-[376px] p-4">
+         <div className="flex flex-col items-start gap-5 h-[772px] w-[100%]">
+           <div className="flex w-[100%] h-[376px] p-4 flex-col items-start gap-4 rounded-[16px] border-2 border-[#88D1D8] bg-[linear-gradient(133deg,_#FFF_1.02%,_#C5E6E9_99.79%)] shadow-[2.769px_6.646px_12.837px_0px_rgba(0,0,0,0.04)]">
   <div 
     className="rounded-lg h-full w-full bg-cover bg-center bg-no-repeat"
     style={{ backgroundImage: `url(${blogImage})` }}
   />
-</div>
+
 
             <div className="flex flex-col items-start gap-4 self-stretch">
               <div class="flex flex-col items-start gap-3 self-stretch">
@@ -52,16 +51,16 @@ const Blog = () => {
 </div>
 </div>
 <div class="flex items-start gap-2 self-stretch">
-<div class="flex items-start mix-blend-multiply w-[182px] h-[24px]">
+<div class="flex items-start mix-blend-multiply ">
 
-<div class="flex justify-center items-center px-[10px] py-[2px] rounded-[16px] bg-[#FEF2E9] w-[182px] h-[24px]">
+<div class="flex justify-center items-center px-[10px] py-[2px] rounded-[16px] bg-[#FEF2E9]">
     <div class="text-[#F57B1F] text-center font-inter text-sm font-medium leading-5">
       Strategy & Management
     </div>
     </div>
 
 </div>
-<div class="flex items-start mix-blend-multiply w-[94px] h-[24px]">
+<div class="flex items-start mix-blend-multiply ">
 
 <div class="flex justify-center items-center px-[10px] py-[2px] rounded-[16px] bg-[#F0F6EC]">
 <div class="text-[#69A042] text-center font-inter text-sm font-medium leading-5">
@@ -74,13 +73,12 @@ const Blog = () => {
 </div>
 
 </div>
-   <div className="flex w-[408px] h-[376px] p-4 flex-col items-start gap-4 rounded-[16px] border-2 border-[#88D1D8] bg-[linear-gradient(133deg,_#FFF_1.02%,_#C5E6E9_99.79%)] shadow-[2.769px_6.646px_12.837px_0px_rgba(0,0,0,0.04)]">
-  <div className="h-[200px] w-[376px] p-4">
+   <div className="flex w-[100%] h-[376px] p-4 flex-col items-start gap-4 rounded-[16px] border-2 border-[#88D1D8] bg-[linear-gradient(133deg,_#FFF_1.02%,_#C5E6E9_99.79%)] shadow-[2.769px_6.646px_12.837px_0px_rgba(0,0,0,0.04)]">
   <div 
     className="rounded-lg h-full w-full bg-cover bg-center bg-no-repeat"
     style={{ backgroundImage: `url(${blogImage})` }}
   />
-</div>
+
 
             <div className="flex flex-col items-start gap-4 self-stretch">
               <div class="flex flex-col items-start gap-3 self-stretch">
@@ -112,16 +110,16 @@ const Blog = () => {
 </div>
 </div>
 <div class="flex items-start gap-2 self-stretch">
-<div class="flex items-start mix-blend-multiply w-[182px] h-[24px]">
+<div class="flex items-start mix-blend-multiply">
 
-<div class="flex justify-center items-center px-[10px] py-[2px] rounded-[16px] bg-[#FEF2E9] w-[182px] h-[24px]">
+<div class="flex justify-center items-center px-[10px] py-[2px] rounded-[16px] bg-[#FEF2E9]">
     <div class="text-[#F57B1F] text-center font-inter text-sm font-medium leading-5">
       Strategy & Management
     </div>
     </div>
 
 </div>
-<div class="flex items-start mix-blend-multiply w-[94px] h-[24px]">
+<div class="flex items-start mix-blend-multiply">
 
 <div class="flex justify-center items-center px-[10px] py-[2px] rounded-[16px] bg-[#F0F6EC]">
 <div class="text-[#69A042] text-center font-inter text-sm font-medium leading-5">
